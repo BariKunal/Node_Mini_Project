10b5e1501568c933a05128e5373a695bc9f0e75a
BariKunal <kunalbari751@gmail.com>
2025-07-04 10:42:16 +0530

    API's For Update Delete and fetch data

--- src/routers/router.js
+++ src/routers/router.js
@@ -7,5 +7,8 @@ let router = express.Router()
 router.get("/",prodCtrl.homePage)
 router.get("/product",prodCtrl.Product)
 router.post("/addProd",prodCtrl.addProducts)
-
+router.get("/allProducts",prodCtrl.Allproduct)
+router.get("/deleteProd",prodCtrl.DeleteProd)
+router.get("/update",prodCtrl.UpdateRender)
+router.post("/ProdUpdt",prodCtrl.UpdtProduct)
 module.exports = router
